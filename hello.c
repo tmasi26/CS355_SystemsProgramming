b91aa45f9c25eac06b3629537a8d1e0302b037dc
Tessa Masi <63470134+tmasi26@users.noreply.github.com>
2025-08-26 12:53:03 -0400

    Add files via upload
    LAB 1

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Your first ever program in C
+
+int main() {                    // serves as the entry point into any C program
+    printf("Hello, world!\n");
+    return 0;                   // indicates a successful completion of the program
+}
+
+
+// Q1: What does \n mean in line 6?
+// Q2: What other characters exist beside \n?
