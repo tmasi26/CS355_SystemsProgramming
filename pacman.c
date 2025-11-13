4cbd0489798646f02f6ed5f3eb844d45f917b8dc
Tessa Masi <63470134+tmasi26@users.noreply.github.com>
2025-11-12 21:32:44 -0500

    Correct syntax and enhance map drawing
    Fix syntax errors and improve map drawing function.

--- pacman.c
+++ pacman.c
@@ -37,6 +37,63 @@ int pacman_x = 17;
 int pacman_y = 3;
 
 
+//DRAW THE MAP
+//mvprintw is an ncurses function that lets you move the cursor 
+//and print text on the screen at the same time
+
+void draw_map(){
+    for(int y = 0; y < ROWS; y++){
+        //prints the value for map[y] at row y, column 0 in the terminal window, so it prints each row
+        mcprintw(y, 0, "%s", map[y]
+    }
+}
+
+//MOVE PAC-MAN IF NO WALL
+void move_placement(int dy int dx){
+    int new_y = pacman_y + dy;
+    int new_x = pacman_x + dx;
+    if (map[new_y][map_x]) != WALL){
+        map[pacman_y][pacman_x] = EMPTY;
+        pacman_y = new_y;
+        pacman_x = new_x;
+        map[pacman_y][pacman_x] = PACMAN;
+    }
+}
+
+int main(){
+    initscr();                //Start ncurses
+    noecho();                 //Do not print key presses
+    curs_set(FALSE);          //Hide cursor
+    keypad(stdscr, TRUE);     //Enable arrow keys
+
+    int ch;
+    while((ch = getch()) != 'q' { //Press 'q' to quit
+        switch (ch){
+            case KEY_UP:
+                move_pacman(-1, 0);
+                break;
+            case KEY_DOWN:
+                move_pacman(1, 0);
+                break;
+            case KEY_LEFT:
+                move_pacman(0, -1);
+                break;
+            case KEY_RIGHT:
+                move_pacman(0, 1);
+                break;
+        }
+        clear();
+        draw_map();
+        refresh();
+    }
+    endwin();
+    return 0;
+}
+
+
+
+
+
   
 
 
