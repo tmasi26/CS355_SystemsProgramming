39515470e575a8b9c5d7d8fd8e184fcd07a6f4d4
Tessa Masi <63470134+tmasi26@users.noreply.github.com>
2025-11-12 21:09:08 -0500

    Initialize Pacman game structure and map
    Initial setup of Pacman game with basic map and symbols.

--- pacman.c
+++ pacman.c
@@ -1,1 +1,43 @@
+//Pacman game for final project CS 355
+//Authors: Tessa Masi, Shiqi Tan
+
+//Determine code for ghost AI, determine how to create the maze. 
+//Want to make a maze that looks like 'CS 355'
+
+
+#include <ncurses.h>
+#include <stdlib.h>
+
+
+//SYMBOLS
+#define WALL '#'
+//want to create a customizable character soon (like Pac-Man w/ a bow)
+#define PACMAN 'C' 
+#define EMPTY ' '
+
+//MAP SIZE
+#define ROWS 10
+#define COLS 20
+
+//CREATE BASIC MAP (global)
+char map[ROWS][COLS + 1] = {
+    "###################",
+    "#        #        #",
+    "# ####   #   #### #",
+    "#                C#",
+    "#### ### ### ### ##",
+    "#                 #",
+    "# ####   #   #### #",
+    "#        #        #",
+    "###################",
+};
+
+//PAC-MAN Position
+int pacman_x = 17;
+int pacman_y = 3;
+
+
+  
+
+
 
