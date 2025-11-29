439e2dfafc57f32183266128935f17f6ba0579fe
Tessa Masi <63470134+tmasi26@users.noreply.github.com>
2025-11-29 10:05:58 -0500

    Changed ghost movement to DFS, Spread out the map, started working towards power ups
    Tried to make a fun "points" box it is not working so far

--- pacman.c
+++ pacman.c
@@ -9,230 +9,293 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-//SYMBOLS
+// SYMBOLS
 #define WALL '#'
-//want to create a customizable character soon (like Pac-Man w/ a bow)
 #define PACMAN 'C' 
 #define EMPTY ' '
+#define POINT '.'
+#define CELL_SIZE 2  // Each corridor step is 2 map units wide
 
-//MAP SIZE
+
+// MAP SIZE
 #define ROWS 30
-#define COLS 30
+#define COLS 60
+
 
 //create ghost
 #define GHOST_AMOUNT 4
 
 //GHOST structure
 typedef struct {
-    int x, y;             
-    int start_x, start_y; //mark start location, use for come back
-    int direction;         
+    int x, y;   
+    int start_x, start_y; 
+    int direction;
     int color_pair;
     int scared;
-    // scared = 0 means normal mode, ghosts can eat pacman
-    // scared =1 means blue mode, pacman can eat ghosts
-    int speed; // this is for adjust different ghosts speed
-    // int after_eaten; temporary cancel this one
-    // after_eaten = 1 means ghosts are already eaten by pacman, and this is helpful to reset the color and normal behavior
-    int move_counter; //count time, make speed slow
+    int speed;
+    int after_eaten;
+    int move_counter;
+    int move_timer;
 } Ghost;
 
-Ghost ghosts[GHOST_AMOUNT];
 
-int blue; // set a whole mode for power-pacman
+//creates # of structs for the # of ghosts needed. 
+Ghost ghosts[GHOST_AMOUNT];
+int blue;
 int powerpellet_time = 0;
 
+int opposite(int dir){
+    if(dir == 0) return 1;
+    if(dir == 1) return 0;
+    if(dir == 2) return 3;
+    return 2;
+}
+
+//ghost direction
+int dx[4] = {0, 0, -1, 1};  
+int dy[4] = {-1, 1, 0, 0};
+
+int running = 1;
+int points = 0;
 
 //CREATE BASIC MAP (global)
 char map[ROWS][COLS+1] = {
-"##############################",
-"#............................#",
-"#.#####.########.######.####.#",
-"#.#####......................#",
-"#.#####.#####.####.###.###.#.#",
-"#.#   #.#####.#  #.###.###.#.#",
-"#.#   #.#   #.## #.###.###.#.#",
-"#.#   #.#   #.## #.###.#...#.#",
-"#.#   #.# ###.## #.#...###.#.#",
-"#.# ###.# ###.#  #.###.###.#.#",
-"#.# ###.# ###.## #.###.###.#.#",
-"#.# ###.#   #.## #.###.###...#",
-"#.# ###.### #.## #.###...#.#.#",
-"#.# ###.### #.## #..##.###.#.#",
-"#.# ###.### #.## #.###.###.#.#",
-"#.#   #.#   #.#  #.###.###.#.#",
-"#.#   #.#   #.####...........#",
-"#.#   #.#   #.......########.#",
-"#.#   #.#####.#.###.########.#",
-"#.#####.#####.#.###.########.#",
-"#.#####.#####.#.###.########.#",
-"#.#####.......#..#...........#",
-"#.........######.#.####.####.#",
-"#.####.##.######.#.####.####.#",
-"#.####.##.######.#.####.####.#",
-"#.####.##.######.#.####.####.#",
-"#.####.##.######.#.####.####.#",
-"#.####.##.######.#.####.####.#",
-"#.........######...####......#",
-"##############################"
+    "###########################################################",
+    "# . . . . . . . . . . . . . . . . . . . . . . . . . . . . #",
+    "# . ######## . ######### . ####### . ################## . #", 
+    "# . ######### . . . . . . . . . . . . . . . . . . . . . . #",
+    "# . ######### . ######### . ####### . ##### . ##### . # . #",
+    "# . ##     ## . ######### . #     # . ##### . ##### . # . #",
+    "# . ##     ## . #       # . ###   # . ##### . ##### . # . #",
+    "# . ##     ## . #       # . ###   # . ##### . # . . . # . #",
+    "# . ##     ## . #   ##### . ###   # . # . . . ##### . # . #",
+    "# . ##  ##### . #   ##### . ##   ## . ##### . ##### . # . #",
+    "# . ##  ##### . #   ##### . ###   # . ##### . ##### . # . #",
+    "# . ##  ##### . #       # . ###   # . ##### . ##### . . . #",
+    "# . ##  ##### . #####   # . ###   # . ##### . . .## . # . #",
+    "# . ##  ##### . #####   # . ###   # . . ### . ##### . # . #",
+    "# . ##  ##### . #####   # . ###   # . ##### . ##### . # . #",
+    "# . ##     ## . #       # . #     # . ##### . ##### . # . #",
+    "# . ##     ## . #       # . ####### . . . . . . . . . . . #",
+    "# . ##     ## . #       # . . . . . . . ############### . #",
+    "# . ##     ## . ######### . # . ##### . ############### . #",
+    "# . ######### . ######### . # . ##### . ############### . #",
+    "# . ######### . ######### . # . ##### . ############### . #",
+    "# . ######### . . . . . . . # . . # . . . . . . . . . . . #",
+    "# . . . . . . . . . ########### . # . ####### . ####### . #",
+    "# . ####### . ### . ########### . # . ####### . ####### . #",
+    "# . ####### . ### . ########### . # . ####### . ####### . #",
+    "# . ####### . ### . ########### . # . ####### . ####### . #",
+    "# . ####### . ### . ########### . # . ####### . ####### . #",
+    "# . ####### . ### . ########### . # . ####### . ####### . #",
+    "# . . . . . . . . . . . . . . . . . . ####### . . . . . . #",  // More open bottom
+    "###########################################################"
 };
 
-
-
-//PAC-MAN Position
+// PAC-MAN Position and movement (global variables)
 int pacman_x = 17;
 int pacman_y = 3;
+int pacman_dx = 0;
+int pacman_dy = 0;
+int pacman_speed = 3;  // Higher number = slower movement
+int pacman_move_counter = 0;
 
-
-//DRAW THE MAP
-//mvprintw is an ncurses function that lets you move the cursor 
-//and print text on the screen at the same time
-
-void draw_map(){
-    for (int y = 0; y < ROWS; y++){
-        for(int x = 0; x < COLS; x++){
+void draw_map() {
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
             char ch = map[y][x];
-            if ( ch == '#'){
+            if (ch == '#') {
                 attron(COLOR_PAIR(1));
-                mvaddch(y, x, ' '); //print a **space** colored blue
+                mvaddch(y, x, ' ');
                 attroff(COLOR_PAIR(1));
-            }
-            else if (ch == 'C'){
+            } else if (ch == 'C') {
                 attron(COLOR_PAIR(2));
-                mvaddch(y, x, PACMAN); // actually draw Pac-Man
+                mvaddch(y, x, PACMAN);
                 attroff(COLOR_PAIR(2));
-            }
-            else{
-                mvaddch(y, x, ch); //free space or dots
+            } else {
+                mvaddch(y, x, ch);
             }
         }
     }
 }
 
-//MOVE PAC-MAN IF NO WALL
-void move_pacman(int dy, int dx){
-    int new_y = pacman_y + dy;
-    int new_x = pacman_x + dx;
-    if ((map[new_y][new_x]) != WALL){
+void draw_point_count(){
+    char points_map[6][200] = {
+    "           ######*   #####*  ########* ###*   ##* ########* ########* ",
+    "           ##***##* ##***##*    ##**** ####*  ##*    ##**** ##******* ",
+    "           ######** ##*  ##*    ##*    ##*##* ##*    ##*    ########* ",
+    "           ##*****  ##*  ##*    ##*    ##**##*##*    ##*    ******##* ",
+    "           ##*       #####*   #######* ##* *####*    ##*    ########* ",
+    "           ***       *****    ******** ***  *****    ***    ********* "
+    };
+    for (int y = 0; y < 6; y++) {
+        for (int x = COLS; x < COLS +70; x++) {
+            char ch = map[y][x];
+            if (ch == '#') {
+                attron(COLOR_PAIR(1));
+                mvaddch(y, x, ' ');
+                attroff(COLOR_PAIR(1));
+            } else if (ch == '*') {
+                //attron(A_DIM);
+                attron(COLOR_PAIR(2));
+                mvaddch(y, x, ' ');
+                attroff(COLOR_PAIR(2));
+                //attroff(A_DIM);
+                
+            }
+        }
+    }
+}
+
+void point_counter(){
+    if (map[pacman_x][pacman_y] == POINT){
+        points++;
+    }
+}
+
+// Modify move_pacman function
+void move_pacman() {
+    pacman_move_counter++;
+    if (pacman_move_counter < pacman_speed) {
+        return;  // Skip movement this frame
+    }
+    pacman_move_counter = 0;
+    
+    int new_x = pacman_x + pacman_dx;
+    int new_y = pacman_y + pacman_dy;
+
+    // Check boundaries and walls
+    if (new_x >= 0 && new_x < COLS && new_y >= 0 && new_y < ROWS && 
+        map[new_y][new_x] != WALL) {
+        if(map[new_y][new_x] == POINT){
+            new_x = new_x;
+        } else if(map[new_y][new_x] == EMPTY && ((map[new_y][new_x-1] == POINT) || (map[new_y][new_x+1]) == POINT) ){
+            if(map[new_y][new_x-1] == POINT){
+                new_x = new_x - 1;
+            } else {
+                new_x = new_x + 1;
+            }
+        }
+        // Clear old position
         map[pacman_y][pacman_x] = EMPTY;
-        pacman_y = new_y;
+        
+        // Update position
         pacman_x = new_x;
+        pacman_y = new_y;
+        
+        // Draw Pac-Man at new position
         map[pacman_y][pacman_x] = PACMAN;
     }
 }
 
-//ghost direction
-    int dx[4] = {0, 0, -1, 1};  
-    int dy[4] = {-1, 1, 0, 0};
-
-
-    //initialize ghosts
-    void initialize_ghosts(){
-        srand(time(NULL));
-        for(int i=0; i<4; i++){
-            int gx, gy;
-            ghosts[i].scared = 1;
-            //make sure get a right position in map
-            do {
-                gx = rand() % (COLS-2) + 1;
-                gy = rand() % (ROWS-2) + 1;
-            } while(map[gy][gx] == WALL || (gx == pacman_x && gy == pacman_y)||map[gy][gx] != '.');
-            //get start
-            ghosts[i].x = ghosts[i].start_x = gx;
-            ghosts[i].y = ghosts[i].start_y = gy;
-            ghosts[i].direction = rand() % 4;
-            ghosts[i].color_pair = i + 4;
-            ghosts[i].speed = 2 + i; //different speed
-            ghosts[i].move_counter = 0;
-        }
+//initialize ghosts
+// In initialize_ghosts(), make ghosts much slower:
+void initialize_ghosts(){
+    srand(time(NULL));
+    for(int i = 0; i < GHOST_AMOUNT; i++){
+        int gx, gy;
+        ghosts[i].scared = 0;  // Start not scared
+        //make sure get a right position in map
+        do {
+            gx = rand() % (COLS-2) + 1;
+            gy = rand() % (ROWS-2) + 1;
+        } while(map[gy][gx] == WALL || (map[gy][gx] == EMPTY));
+        ghosts[i].x = ghosts[i].start_x = gx;
+        ghosts[i].y = ghosts[i].start_y = gy;
+        ghosts[i].direction = rand() % 4;
+        ghosts[i].color_pair = i + 4;
+        ghosts[i].speed = 3 + i*2;  // Much higher numbers = slower ghosts
+        ghosts[i].move_counter = 0;
+        ghosts[i].after_eaten = 0;
     }
+}
 
-
-    int opposite(int dir){
-        if(dir == 0) return 1;
-        if(dir == 1) return 0;
-        if(dir == 2) return 3;
-        return 2;
-    }
-
-    //Ghosts movement
-    void move_ghosts(int i){
-            //next position
-            int nx = ghosts[i].x + dx[ghosts[i].direction];
-            int ny = ghosts[i].y + dy[ghosts[i].direction];
-            //no wall, keep going
-            if(map[ny][nx] != WALL){
-                ghosts[i].x = nx;
-                ghosts[i].y = ny;
-            } else {
-                //change direction
-                int possible[4];
-                int count=0;
-                for(int d=0; d<4; d++){
-                    //reduce go back and forth
-                    if(d == opposite(ghosts[i].direction)){
-                        continue; 
-                    }
-                    //test all direction 
-                    int tx = ghosts[i].x + dx[d];
-                    int ty = ghosts[i].y + dy[d];
-                    if(map[ty][tx] != WALL) possible[count++] = d;
-                }
-                if(count > 0){
-                    ghosts[i].direction = possible[rand()%count];
-                    ghosts[i].x += dx[ghosts[i].direction];
-                    ghosts[i].y += dy[ghosts[i].direction];
-                } else {
-                    ghosts[i].direction = opposite(ghosts[i].direction);
-                }
-            }
-    }
-
-            //pacman encounter ghosts
-            void check_encounter(){
-            for (int i = 0; i< 4; i++){
-            if(ghosts[i].x == pacman_x && ghosts[i].y == pacman_y){
-                //if power pellets, blue mode
-                if(ghosts[i].scared){
-                    ghosts[i].x = ghosts[i].start_x;
-                    ghosts[i].y = ghosts[i].start_y;
-                    ghosts[i].scared = 0;
-                    ghosts[i].speed = 2 + i; //different speed
-                    ghosts[i].move_counter = 0;
-                    ghosts[i].direction = rand()%4;
-                    continue;
-                }else{
-                    //no power pellets
-                    mvprintw(ROWS+1, 0, "Game-Over!");
-                    refresh();
-                    getch();
-                    endwin();
-                    exit(0);
-                }
-
-               
-            }
-        }
-    }
-
-    //DRAW GHOSTS
-    void draw_ghosts(){
-        for(int i=0; i<GHOST_AMOUNT; i++){
-            attron(COLOR_PAIR(ghosts[i].color_pair));
-            mvaddch(ghosts[i].y, ghosts[i].x, 'G'); 
-            attroff(COLOR_PAIR(ghosts[i].color_pair));
-        }
-    }
-
-    //blue mode
-    void blue_ghosts() {
+//Ghosts movement
+void move_ghosts() {
     for(int i = 0; i < GHOST_AMOUNT; i++) {
-        
-       // if(ghosts[i].after_eaten) {
-            //ghosts[i].color_pair = i + 4;
-        //} 
-        if(blue == 1 && ghosts[i].scared) {
+        ghosts[i].move_timer++;
+        // Only move when timer reaches speed
+        if (ghosts[i].move_timer < ghosts[i].speed) {
+            continue;
+        }
+        // --- Ghost chooses direction ---
+        int best_dir = -1;
+        int min_dist = 1000;
+        for(int d = 0; d < 4; d++) {
+            int nx = ghosts[i].x + dx[d];
+            int ny = ghosts[i].y + dy[d];
+
+            if(nx >= 0 && nx < COLS && ny >= 0 && ny < ROWS && map[ny][nx] != WALL) {
+                int dist = abs(nx - pacman_x) + abs(ny - pacman_y);
+
+                if(dist < min_dist) {
+                    min_dist = dist;
+                    best_dir = d;
+                }
+            }
+        }
+        // --- Move ghost ---
+        if (best_dir != -1) {
+            ghosts[i].x += dx[best_dir];
+            ghosts[i].y += dy[best_dir];
+        }
+        // Reset timer
+        ghosts[i].move_timer = 0;
+    }
+}
+
+//pacman encounter ghosts
+void check_encounter(){
+    for (int i = 0; i < 4; i++){
+        if(ghosts[i].x == pacman_x && ghosts[i].y == pacman_y){
+            //if power pellets, blue mode
+            if(ghosts[i].scared){
+                ghosts[i].x = ghosts[i].start_x;
+                ghosts[i].y = ghosts[i].start_y;
+                ghosts[i].scared = 0;
+                ghosts[i].speed = 2 + i; //different speed
+                ghosts[i].move_counter = 0;
+                ghosts[i].direction = rand()%4;
+                continue;
+            } else{
+                //no power pellets
+                mvprintw(ROWS+1, 0, "Game-Over!");
+                refresh();
+                getch();
+                endwin();
+                exit(0);
+            }   
+        }
+    }
+}
+
+//DRAW GHOSTS
+void draw_ghosts(){
+    for(int i=0; i < GHOST_AMOUNT; i++){
+        attron(COLOR_PAIR(ghosts[i].color_pair));
+        mvaddch(ghosts[i].y, ghosts[i].x, 'G'); 
+        attroff(COLOR_PAIR(ghosts[i].color_pair));
+    }
+}
+
+void power_pellets(){
+    int rx, ry;
+    if (points % 25 == 0){
+        do {
+                rx = rand() % (COLS-2) + 1;
+                ry = rand() % (ROWS-2) + 1;
+            } while(map[ry][rx] == WALL || (map[ry][rx] == EMPTY));
+        mvaddch(ry, rx, '@');
+    }
+}
+
+//blue mode
+void blue_ghosts() {
+    for(int i = 0; i < GHOST_AMOUNT; i++) {
+        // 1. after eaten by pacman ,recover
+        if(ghosts[i].after_eaten) {
+            ghosts[i].color_pair = i + 4;
+        } else if(blue == 1 && ghosts[i].scared) {
             // 2. blue mode
             ghosts[i].color_pair = 8;
         } else {
@@ -242,83 +305,88 @@ void move_pacman(int dy, int dx){
     }
 }
 
+int main() {
+    initscr();
+    noecho();
+    curs_set(FALSE);
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);  // Make getch() non-blocking
+    start_color();
+    use_default_colors();
 
-int main(){
-    initscr();                //Start ncurses
-    noecho();                 //Do not print key presses
-    curs_set(FALSE);          //Hide cursor
-    keypad(stdscr, TRUE);     //Enable arrow keys
-    start_color();            //enable colors
-    use_default_colors();     //Optional: use terminal default
+    init_pair(1, COLOR_BLUE, COLOR_BLUE);
+    init_pair(2, COLOR_YELLOW, -1);
+    init_pair(3, COLOR_WHITE, -1);
+    // Add color pairs for ghosts
+    init_pair(4, COLOR_RED, -1);
+    init_pair(5, COLOR_CYAN, -1);
+    init_pair(6, COLOR_MAGENTA, -1); 
+    init_pair(7, COLOR_GREEN, -1);
+    init_pair(8, COLOR_BLUE, -1);  // For scared ghosts
 
-    init_pair(1, COLOR_BLUE, COLOR_BLUE);       //walls: blue on blue
-    init_pair(2, COLOR_YELLOW, -1);             //pacman: yellow foreground, default background
-    init_pair(3, COLOR_WHITE, -1);              //Free space: white
 
-    init_pair(4, COLOR_RED, -1);                // ghost 1 
-    init_pair(5, COLOR_CYAN, -1);               // ghost 2 
-    init_pair(6, COLOR_MAGENTA, -1);            // ghost 3
-    init_pair(7, COLOR_GREEN, -1);              // ghost 4
-    init_pair(8, COLOR_BLUE, -1);               //scared mode
 
+    // Initialize Pac-Man position
+    map[pacman_y][pacman_x] = PACMAN;
     initialize_ghosts();
+    draw_point_count();
 
-    blue = 1; //test blue mode
-    
-
-    int ch;
-    while((ch = getch()) != 'q') { //Press 'q' to quit
-        switch (ch){
-            case KEY_UP:
-                move_pacman(-1, 0);
+    while (running) {
+        int ch = getch();
+        
+        // Handle input
+        switch(ch) {
+            case KEY_UP:    
+                pacman_dy = -1; 
+                pacman_dx = 0;
+                check_encounter(); 
+                break;
+            case KEY_DOWN:  
+                pacman_dy = 1;  
+                pacman_dx = 0; 
                 check_encounter();
                 break;
-            case KEY_DOWN:
-                move_pacman(1, 0);
+            case KEY_LEFT:  
+                pacman_dx = -1; 
+                pacman_dy = 0; 
                 check_encounter();
                 break;
-            case KEY_LEFT:
-                move_pacman(0, -1);
+            case KEY_RIGHT: 
+                pacman_dx = 1;  
+                pacman_dy = 0; 
                 check_encounter();
                 break;
-            case KEY_RIGHT:
-                move_pacman(0, 1);
+            case 'q':       
+                running = 0; 
                 check_encounter();
                 break;
         }
+        
+        // Move ghosts (they're already slowed by their speed counters)
         for(int i = 0; i < GHOST_AMOUNT; i++){
             ghosts[i].move_counter++;
-            // slow speed, mover until counter
             if(ghosts[i].move_counter >= ghosts[i].speed){
-                move_ghosts(i);
-                check_encounter();
+                move_ghosts(i);  
                 ghosts[i].move_counter = 0;
-    }
-}
-        // blue mode timer check
-        if (blue == 1 && powerpellet_time == 0){
-            powerpellet_time = time(NULL); // start timer when first triggered
+            }
         }
-    
-        if (blue == 1 && time(NULL) - powerpellet_time >= 10){
-            blue = 0;              // turn off blue mode
-            powerpellet_time = 0;  // reset timer
-                     
-        }
+        
+        // Move Pac-Man (now slowed by his speed counter)
+        move_pacman();
+        power_pellets();
+        
+        
         blue_ghosts();
         clear();
         draw_map();      
-        draw_ghosts();   
+        draw_ghosts();  
+        //draw_point_count(); 
         refresh();
-        napms(180);
+        napms(50);  // You can adjust this for overall game speed
     }
     endwin();
     return 0;
 }
-
-
-
-
 
 
 
